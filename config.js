95980f712585c35d2e3de99dbd3987c2725d9241
InfinityYoutube2 <168295460+InfinityYoutube2@users.noreply.github.com>
2024-04-27 20:17:24 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+ "TOKEN": '', // Bot Tokeni.
+ "GUILD_ID": '',  // Sunucu (guild) ID'si.
+ "ROLE_ID": '',    // Hedef rol ID'si.
+ "EXPECTED_STATUS": 'client', // Kişilerin durumu.
+ "CHANNEL_ID": '', // Resim kanalı ID'si.
+ "EMOJI": '', // Resimlerin altına eklenecek emoji ID si.
+ "STATUS": [
+      "client",
+    ".gg/client",
+      "client",
+    ".gg/client",
+    "bıdı <3 client"
+] // Botun oynuyor kısmı.
+ 
+  };
